fb2278b9be793c4a7bfca532f37fd01e8f390b4f
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2020-03-08 20:40:42 -0400

    working

--- src/main/java/bin2dec/App.java
+++ src/main/java/bin2dec/App.java
@@ -8,14 +8,14 @@ public class App {
     }
 
     public int convert(String binaryNumber) {
-        int sum = 0;
+        int decimal = 0;
         int position = binaryNumber.length() - 1;
         for (int i = 0; i < binaryNumber.length(); i++) {
             char character = binaryNumber.charAt(i);
             int binary = Character.getNumericValue(character);
-            sum += binary * Math.pow(2, position);
+            decimal += binary * Math.pow(2, position);
             position--;
         }
-        return sum;
+        return decimal;
     }
 }
