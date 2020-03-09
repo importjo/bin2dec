beab1a6c7bff6c6d46137397f58289ab4e9fc3e7
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2020-03-08 20:52:53 -0400

    working

--- src/main/java/bin2dec/App.java
+++ src/main/java/bin2dec/App.java
@@ -3,16 +3,15 @@ package bin2dec;
 import java.lang.Math;
 
 public class App {
-
+  
     public static void main(String[] args) {
     }
 
-    public int convert(String binaryNumber) {
+    public int convert(String number) {
         int decimal = 0;
-        int position = binaryNumber.length() - 1;
-        for (int i = 0; i < binaryNumber.length(); i++) {
-            char character = binaryNumber.charAt(i);
-            int binary = Character.getNumericValue(character);
+        int position = number.length() - 1;
+        for (int i = 0; i < number.length(); i++) {
+            int binary = Character.getNumericValue(number.charAt(i)); 
             decimal += binary * Math.pow(2, position);
             position--;
         }
